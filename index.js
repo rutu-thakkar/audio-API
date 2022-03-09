66ed1c62f0a136541ce7bb99b401eae1513196a2
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-03-09 16:49:26 +0530

    get single file

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.removeHeader("x-powered-by");
   //set the allowed HTTP methods to be requested
-  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET");
   //headers clients can use in their requests
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   //allow request to continue and be handled by routes
