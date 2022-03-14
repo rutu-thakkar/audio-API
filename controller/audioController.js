a781b433894e3a8ca4c723af0ca91b16906f8bed
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-03-14 12:24:54 +0530

    Added fileUpload

--- controller/audioController.js
+++ controller/audioController.js
@@ -5,12 +5,12 @@ require("dotenv").config();
 const cloudinary = require("cloudinary");
 const streamifier = require("streamifier");
 const { getAudioDurationInSeconds } = require("get-audio-duration");
-// route.use(fileUpload());
-
-// {
-//     useTempFiles: true,
-//     tempFileDir: "/tmp/",
-//   }
+route.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 cloudinary.config({
   cloud_name: "rutu",
@@ -29,46 +29,59 @@ const getAudios = (req, res) => {
 };
 
 const addAudio = (req, res) => {
-  if (req.file) {
-    var file = req.file;
-    console.log(file);
-    if (
-      req.file.mimetype === "audio/basic" ||
-      req.file.mimetype === "audio/mpeg" ||
-      req.file.mimetype === "audio/vnd.wav" ||
-      req.file.mimetype === "audio/vorbis" ||
-      req.file.mimetype === "audio/ogg"
-    ) {
-      getAudioDurationInSeconds(req.file.path)
-        .then((duration) => {
-          // if (duration < 60) {
-          //   duration = duration;
-          //   duration = duration.toFixed(3) + " seconds";
-          // } else if (duration > 60) {
-          //   duration = duration / 60;
-          //   duration = duration.toFixed(2) + " minutes";
-          // }
+  console.log(req.files);
 
-          db.audioDetails
-            .create({
-              filename: Date.now() + "-" + req.file.originalname,
-              fileURL: req.file.path,
-              duration: duration,
-            })
-            .then((data) => {
-              res.json({ message: "File Uploaded!", data });
-            })
-            .catch((err) => {
-              res.json(err);
-            });
-        })
-        .catch((err) => {
-          // res.json(err);
-          res.send(err.message);
-        });
-    } else {
-      res.json({ message: "File type must be audio" });
-    }
+  if (req.files) {
+    var file = req.files["audioFile"];
+    console.log(file);
+    // var fileName = req.files["audioFile"].name;
+
+    file.mv("./assets/uploads/" + req.files["audioFile"].name, function (err) {
+      if (err)
+        return res
+          .status(400)
+          .send(JSON.stringify({ failed: "Something went wrong" }));
+    });
+  } else {
+    return res.status(400).send(JSON.stringify({ failed: "No such File" }));
+  }
+  // return res.status(200).send('{"status":"success"}');
+
+  if (
+    req.files["audioFile"].mimetype === "audio/basic" ||
+    req.files["audioFile"].mimetype === "audio/mpeg" ||
+    req.files["audioFile"].mimetype === "audio/vnd.wav" ||
+    req.files["audioFile"].mimetype === "audio/vorbis" ||
+    req.files["audioFile"].mimetype === "audio/ogg"
+  ) {
+    getAudioDurationInSeconds("./assets/uploads/" + req.files["audioFile"].name)
+      .then((duration) => {
+        // if (duration < 60) {
+        //   duration = duration;
+        //   duration = duration.toFixed(3) + " seconds";
+        // } else if (duration > 60) {
+        //   duration = duration / 60;
+        //   duration = duration.toFixed(2) + " minutes";
+        // }
+        db.audioDetails
+          .create({
+            filename: Date.now() + "-" + req.files["audioFile"].name,
+            fileURL: "./assets/" + req.files["audioFile"].name,
+            duration: duration,
+          })
+          .then((data) => {
+            res.json({ message: "File Uploaded!", data });
+          })
+          .catch((err) => {
+            res.json(err);
+          });
+      })
+      .catch((err) => {
+        // res.json(err);
+        res.send(err.message);
+      });
+  } else {
+    res.json({ message: "File type must be audio" });
   }
 };
 
